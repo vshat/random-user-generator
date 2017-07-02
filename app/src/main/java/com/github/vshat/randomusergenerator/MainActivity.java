77658d8d19615173daa5d7475fa0be94b7aad040
vadymsh <vadym.shatokhin@gmail.com>
2017-07-02 12:07:45 +0300

    add stub list and stub user detail activity

--- app/src/main/java/com/github/vshat/randomusergenerator/MainActivity.java
+++ app/src/main/java/com/github/vshat/randomusergenerator/MainActivity.java
@@ -1,13 +1,50 @@
 package com.github.vshat.randomusergenerator;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements UsersAdapter.OnItemClickListener {
+
+    private static List<String> createStringList() {
+        List<String> result = new ArrayList<>(10);
+
+        for (int i = 0; i < 10; i++) {
+            result.add(i, "User #" + i);
+        }
+
+        return result;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        UsersAdapter usersAdapter = new UsersAdapter(createStringList());
+        usersAdapter.setOnItemClickListener(this);
+
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recyclerview_main);
+
+        recyclerView.setAdapter(usersAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+
     }
+
+    @Override
+    public void onItemClick(View itemView, int position) {
+        UserDetailActivity.start(this, ((TextView)itemView.findViewById(R.id.textview_user_name)).getText().toString());
+    }
+
+
 }
